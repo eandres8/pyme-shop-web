5b47b9fb10da7bf53be13c996f538081403bca18
Andres Vega <elb.andres8@gmail.com>
2026-04-26 17:35:42 -0500

    add category filtered page

--- app/(shop)/category/[id]/page.tsx
+++ app/(shop)/category/[id]/page.tsx
@@ -1,5 +1,23 @@
 import { notFound } from "next/navigation";
 
+import { Product } from "@/src/core/entities";
+import { initialData } from "@/src/data/seed/seed";
+import { ProductGrid } from "@/src/shared/components/product";
+import { Title } from "@/src/shared/components/ui";
+
+const products = initialData.products.map((p) =>
+  Product.fromJson({
+    ...p,
+    in_stock: p.inStock,
+  }),
+);
+
+const subtitles: Record<string, string> = {
+  men: 'Hombres',
+  women: 'Mujeres',
+  kid: 'Niños',
+};
+
 type Props = {
   params: Promise<{ id: string }>;
 };
@@ -7,14 +25,18 @@ type Props = {
 export default async function Category({ params }: Props) {
   const { id } = await params;
 
-  if (!['men', 'women', 'kids'].includes(id)) {
+  if (!['men', 'women', 'kid'].includes(id)) {
     notFound();
   }
 
+  const filteredProducts = products.filter((p) => p.gender === id);
+
   return (
-    <div>
-      <h1>Category Page</h1>
-      <h2>{id}</h2>
-    </div>
+    <>
+      <Title title={`Artículos de ${subtitles[id]}`} subtitle="Todos los productos" className="mb-2" />
+      <ProductGrid
+        products={filteredProducts}
+      />
+    </>
   ); 
 }
