ec23bc78381bc4aeb0ef9f4c182d447e82286da0
Andres Vega <elb.andres8@gmail.com>
2026-06-08 17:20:10 -0500

    feat: add user schema

---
+++ prisma/migrations/20260608221814_user_role/migration.sql
@@ -0,0 +1,20 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('admin', 'user');
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "emailVerified" TIMESTAMP(3),
+    "password" TEXT NOT NULL,
+    "role" "Role" NOT NULL DEFAULT 'user',
+    "image" TEXT,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
